cb1f2a29a68c146b2557512a2fb2b68b378a5259
Timon <19689305+timonvdvelde@users.noreply.github.com>
2020-02-05 00:03:49 +0100

    init

---
+++ formative2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+  static boolean isLeapYear(int year) {
+    // Complete this method.
+  }
+
+  public static void main(String[] args) {
+    // Use the isLeapYear() method to complete the assignment.
+  }
+}
+
